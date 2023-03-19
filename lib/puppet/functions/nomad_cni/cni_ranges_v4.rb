b35bda6d2d3a2d68f225d0bc540ffda26716880f
Massimiliano Adamo <maxadamo@gmail.com>
2023-03-19 15:05:14 +0100

    allow wider subnet

--- lib/puppet/functions/nomad_cni/cni_ranges_v4.rb
+++ lib/puppet/functions/nomad_cni/cni_ranges_v4.rb
@@ -1,14 +1,24 @@
+require 'ipaddr'
 # Function to split a network by a given number of hosts (Nomad agents)
 #
 # network_address: String in CIDR notation (e.g. "192.168.0.0/24")
 #
 # agent_names: Array of strings containing the names of the Nomad agents
 #
-# Returns: Array of arrays, and each array contains the IP of the VXLAN on the
-#          host, the start number for the range and the end number for the range
+# Returns: Array of arrays, and each array contains:
+#        - the name of the agent
+#        - the gateway of the VXLAN on the host
+#        - the first usable number for the range (after the gateway)
+#        - the last usable number for the range
+#        - the netmask
 #
-# Example usage: nomad_cni::cni_ranges_v4("192.168.0.0/24", ["agent1.example.org", "agent2.example.org", "agent3.example.org"]])
-#                returns [[1, 2, 84], [85, 86, 168], [169, 170, 252]]
+#
+# Example: nomad_cni::cni_ranges_v4("192.168.0.0/24", ["agent1.foo.org", "agent2.foo.org", "agent3.foo.org"]])
+#          returns [
+#                    ["agent1.foo.org", 192.168.0.1, 192.168.0.2, 192.168.0.84, 24],
+#                    ["agent2.foo.org", 192.168.0.85, 192.168.0.86, 192.168.0.168, 24],
+#                    ["agent3.foo.org", 192.168.0.169, 192.168.0.170, 192.168.0.252, 24]
+#                  ]
 #
 Puppet::Functions.create_function(:'nomad_cni::cni_ranges_v4') do
   dispatch :calculate_cni_ranges_v4 do
@@ -20,19 +30,19 @@ Puppet::Functions.create_function(:'nomad_cni::cni_ranges_v4') do
   def calculate_cni_ranges_v4(network_address, agent_names)
     netmask = network_address.split('/')[1].to_i
     ip = network_address.split('/')[0]
-    net_prefix = ip.split('.')[0...-1].join('.')
-    raise Puppet::Error, 'this function does not work with subnets greater than 24' if netmask > 24
+    last_ip_integer = IPAddr.new(network_address).to_range.last.to_i
+    first_ip_integer = IPAddr.new(network_address).to_range.first.to_i
+    free_hosts = last_ip_integer - first_ip_integer - 1
 
-    free_hosts = 2**(32 - netmask) - 2
-    agents = agent_names.length
-    chunk_size = (free_hosts / agents).floor
-    agents_array = (0..agents - 1).to_a
+    agent_number = agent_names.length
+    chunk_size = (free_hosts / agent_number).floor
+    agents_array = (0..agent_number - 1).to_a
     agents_array.map do |item|
       [
         agent_names[item],
-        "#{net_prefix}." + (item * chunk_size + 1).to_s,
-        "#{net_prefix}." + (item * chunk_size + 2).to_s,
-        "#{net_prefix}." + (item * chunk_size + chunk_size).to_s,
+        IPAddr.new((first_ip_integer + (chunk_size * item) + 1).to_i, Socket::AF_INET).to_s
+        IPAddr.new((first_ip_integer + (chunk_size * item) + 2).to_i, Socket::AF_INET).to_s
+        IPAddr.new((first_ip_integer + (chunk_size * item) + chunk_size).to_i, Socket::AF_INET).to_s
         netmask,
       ]
     end
