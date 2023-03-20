dfa2ad7bc99e8e24b71835d2226bc660decb7541
Massimiliano Adamo <maxadamo@gmail.com>
2023-03-20 13:08:45 +0100

    add assert_private

--- manifests/reload_service.pp
+++ manifests/reload_service.pp
@@ -2,6 +2,10 @@
 #
 #
 class nomad_cni::reload_serv {
+  # == this is a private class
+  #
+  assert_private()
+
   exec { "${module_name} reload nomad service":
     path        => ['/bin', '/usr/bin'],
     command     => 'systemctl reload nomad',
