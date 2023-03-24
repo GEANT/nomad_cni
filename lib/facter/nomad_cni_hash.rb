071388e5252edd4b8653c6b1c95794a926906e02
Massimiliano Adamo <maxadamo@gmail.com>
2023-03-24 18:42:54 +0100

    add custom fact to enumerate CNIs

---
+++ lib/facter/nomad_cni_hash.rb
@@ -0,0 +1,19 @@
+Facter.add('nomad_cni_hash') do
+  # create a hash containing the name of the CNI and Vxlan ID
+  #
+  # Example: {"cni" => "123456", "cni2" => "78901234"}
+  #
+  setcode do
+    begin
+      cni_hash_output = {}
+      cni_scripts = Dir.glob('/etc/vxlan/*cast.d/*.sh')
+      cni_scripts.each do |cni_script|
+        cni_name = File.basename(cni_script, '.sh')
+        vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
+        cni_hash_output[cni_name] = vxlan_id
+        cni_hash_output
+    rescue
+      {}
+    end
+  end
+end
