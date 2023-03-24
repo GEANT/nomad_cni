095c0597dff7014e94926307df4c616d3e5b887b
Massimiliano Adamo <maxadamo@gmail.com>
2023-03-24 18:44:40 +0100

    add closing end to ruby script

--- lib/facter/nomad_cni_hash.rb
+++ lib/facter/nomad_cni_hash.rb
@@ -4,16 +4,15 @@ Facter.add('nomad_cni_hash') do
   # Example: {"cni" => "123456", "cni2" => "78901234"}
   #
   setcode do
-    begin
-      cni_hash_output = {}
-      cni_scripts = Dir.glob('/etc/vxlan/*cast.d/*.sh')
-      cni_scripts.each do |cni_script|
-        cni_name = File.basename(cni_script, '.sh')
-        vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
-        cni_hash_output[cni_name] = vxlan_id
-        cni_hash_output
-    rescue
-      {}
+    cni_hash_output = {}
+    cni_scripts = Dir.glob('/etc/vxlan/*cast.d/*.sh')
+    cni_scripts.each do |cni_script|
+      cni_name = File.basename(cni_script, '.sh')
+      vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
+      cni_hash_output[cni_name] = vxlan_id
     end
+    cni_hash_output
+  rescue
+    {}
   end
 end
