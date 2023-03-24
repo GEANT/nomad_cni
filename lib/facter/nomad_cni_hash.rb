84946b5d5678aaf965c99f9a6506412203a39341
Massimiliano Adamo <maxadamo@gmail.com>
2023-03-24 22:05:09 +0100

    fix fact key

--- lib/facter/nomad_cni_hash.rb
+++ lib/facter/nomad_cni_hash.rb
@@ -8,6 +8,7 @@ Facter.add('nomad_cni_hash') do
     cni_scripts = Dir.glob('/etc/vxlan/*cast.d/*.sh')
     cni_scripts.each do |cni_script|
       cni_name = File.basename(cni_script, '.sh')
+      vxlan_network = File.read(cni_script).match(%r{^vxlan_network="(.*)"})[1]
       vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
       cni_hash_output[cni_name] = vxlan_id
     end
