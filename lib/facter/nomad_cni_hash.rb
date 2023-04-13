01fc89b3237c2675f6130fa368cca81fd64ae4bb
Massimiliano Adamo <maxadamo@gmail.com>
2023-04-13 23:33:30 +0200

    fix fact against rubocop

--- lib/facter/nomad_cni_hash.rb
+++ lib/facter/nomad_cni_hash.rb
@@ -1,26 +1,31 @@
-Facter.add('nomad_cni_hash') do
-  # create a hash containing the name of the CNI and the subkeys Vxlan ID and network
-  #
-  # Example: {
-  #   'cni1' => { 'id' => '1233456', 'network' => '192.168.20.0/24' },
-  #   'cni2' => { 'id' => '5678901', 'network' => '192.168.10.0/24' }
-  # }
-  #
+# frozen_string_literal: true
+
+require 'facter'
+
+# This facter returns a hash containing the name of the CNI and the subkeys Vxlan ID and network
+#
+# Example: {
+#   'cni1' => { 'id' => '1233456', 'network' => '192.168.20.0/24' },
+#   'cni2' => { 'id' => '5678901', 'network' => '192.168.10.0/24' }
+# }
+#
+Facter.add(:nomad_cni) do
+  confine kernel: 'Linux'
   setcode do
-      cni_hash = {}
-      cni_scripts = Dir.glob('/opt/cni/vxlan/*cast.d/*.sh')
-      cni_scripts.select! {|cni| not cni.end_with?('_bridge_fdb.sh') }
-      cni_scripts.each do |cni_script|
-        cni_name = File.basename(cni_script, '.sh')
-        vxlan_network = File.read(cni_script).match(%r{^vxlan_network="(.*)"})[1]
-        vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
-        cni_hash.merge!({
-                          cni_name => {
-                            'id' => vxlan_id,
-                           'network' => vxlan_network
-                          }
-                        })
-      end
+    cni_hash = {}
+    cni_scripts = Dir.glob('/opt/cni/vxlan/*cast.d/*.sh')
+    cni_scripts.reject! { |cni| cni.end_with?('_bridge_fdb.sh') }
+    cni_scripts.each do |cni_script|
+      cni_name = File.basename(cni_script, '.sh')
+      vxlan_network = File.read(cni_script).match(%r{^vxlan_network="(.*)"})[1]
+      vxlan_id = File.read(cni_script).match(%r{^vxlan_id=(\d+)})[1]
+      cni_hash.merge!({
+                        cni_name => {
+                          'id' => vxlan_id,
+                         'network' => vxlan_network
+                        }
+                      })
+    end
     cni_hash
   rescue
     {}
