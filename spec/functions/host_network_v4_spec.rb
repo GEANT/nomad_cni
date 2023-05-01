b460e764752b666cac9a2ce4198a532aad4f9b34
Massimiliano Adamo <maxadamo@gmail.com>
2023-05-01 22:25:39 +0200

    fix spec test

--- spec/functions/host_network_v4_spec.rb
+++ spec/functions/host_network_v4_spec.rb
@@ -9,7 +9,7 @@ describe 'nomad_cni::host_network_v4' do
         { 'test_cni_1' => { 'cidr' => '192.168.2.1/24', 'interface' => 'vxbr11882895' } },
         { 'test_cni_2' => { 'cidr' => '192.168.3.1/24', 'interface' => 'vxbr5199537' } },
         { 'test_cni_3' => { 'cidr' => '192.168.4.1/24', 'interface' => 'vxbr15782095' } },
-        { 'public' => { 'cidr' => '172.16.254.254/16', 'interface' => 'eth0' } },
+        { 'public_v4' => { 'cidr' => '172.16.254.254/16', 'interface' => 'eth0' } },
       ],
     )
   }
