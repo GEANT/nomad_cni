877d1e33f55c2c2f9daf50f150307d904ff3bdfc
Massimiliano Adamo <maxadamo@gmail.com>
2024-04-25 15:05:59 +0100

    fix forward rules

--- manifests/ingress/firewall.pp
+++ manifests/ingress/firewall.pp
@@ -18,12 +18,12 @@ class nomad_cni::ingress::firewall (
       proto  => 'all',
       chain  => 'FORWARD',
       action => 'accept';
-    "200 allow forward on VXLAN iniface ${interface}":
-      iniface  => 'vx+',
-      outiface => $interface;
     "200 allow forward on Bridge iniface ${interface}":
       iniface  => 'br+',
       outiface => $interface;
+    '200 allow forward on Bridge':
+      iniface  => 'br+',
+      outiface => 'br+';
     "200 Allow VRRP inbound from ${peer_ip}":
       proto  => ['vrrp', 'igmp'],
       chain  => 'INPUT',
